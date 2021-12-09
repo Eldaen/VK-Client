19fa296a67ab9dba830a6e1c19767f0f112b9d99
Sizov Denis <sv_skatch@bk.ru>
2021-12-09 17:44:29 +0300

    Some NewsService refactoring

--- VK client/Service/NewsService.swift
+++ VK client/Service/NewsService.swift
@@ -40,71 +40,9 @@ final class NewsService: NewsLoader {
 							   params: params) { [weak self] (result: Result<NewsMainResponse, Error>) in
 			switch result {
 			case .success(let newsResponse):
-				let items = newsResponse.response.items
-				let groups = newsResponse.response.groups
-				let users = newsResponse.response.profiles
-				var news: [NewsTableViewCellModel] = []
-				var source: NewsSourceProtocol = UserModel()
-				
-				for post in items {
-					
-					if post.postType == nil {
-						continue
-					}
-					
-					let sourceID = post.sourceID
-					let text = post.text
-					
-					if sourceID < 0 {
-						for group in groups {
-							if group.id == sourceID.magnitude {
-								source = group
-							}
-						}
-					} else {
-						for user in users {
-							if user.id == sourceID {
-								source = user
-							}
-						}
-					}
-					
-					let date = Date(timeIntervalSince1970: Double(post.date))
-					var imageLinksArray: [String]? = []
-					
-					if let images = post.photos?.items {
-						imageLinksArray = self?.sortImage(by: "x", from: images)
-					}
-					
-					if let attachments = post.attachments {
-						var images = [ApiImage]()
-						
-						for attachment in attachments {
-							if let image = attachment.photo {
-								images.append(image)
-							}
-							
-							if let link = attachment.link {
-								if let photo = link.photo {
-									images.append(photo)
-								}
-							}
-						}
-					
-						imageLinksArray = self?.sortImage(by: "z", from: images)
-					}
-					
-					let newsModel = NewsTableViewCellModel(
-						source: source,
-						postDate: date.description,
-						postText: text ?? "",
-						newsImageNames: imageLinksArray ?? [],
-						likesModel: post.likes
-					)
-					news.append(newsModel)
+				if let news = self?.configureAnswer(newsResponse) {
+					completion(news)
 				}
-				
-				completion(news)
 			case .failure(let error):
 				debugPrint("Error: \(error.localizedDescription)")
 			}
@@ -178,6 +116,83 @@ final class NewsService: NewsLoader {
 		}
 		
 		return imageLinks
+	}
+}
+
+// MARK: - Private methods
+
+private extension NewsService {
+	
+	/// Cобирает ответ сервера в массив нужных моделей
+	/// - Returns: Массив моделей ячеек таблицы новостей
+	func configureAnswer(_ newsResponse: NewsMainResponse) -> [NewsTableViewCellModel] {
+		let items = newsResponse.response.items
+		let groups = newsResponse.response.groups
+		let users = newsResponse.response.profiles
+		var news: [NewsTableViewCellModel] = []
+		var source: NewsSourceProtocol = UserModel()
+		
+		for post in items {
+			if post.postType == nil {
+				continue
+			}
+			
+			let date = Date(timeIntervalSince1970: Double(post.date))
+			let sourceID = post.sourceID
+			let text = post.text
+			
+			// Выясняем, от группы или от пользователя новость
+			if sourceID < 0 {
+				for group in groups {
+					if group.id == sourceID.magnitude {
+						source = group
+					}
+				}
+			} else {
+				for user in users {
+					if user.id == sourceID {
+						source = user
+					}
+				}
+			}
+			
+			// Вытаскиваем нужные картинки
+			var imageLinksArray: [String]? = []
+			
+			// Если есть фото, то нам нужны фото
+			if let images = post.photos?.items {
+				imageLinksArray = sortImage(by: "z", from: images)
+			}
+			
+			// Если есть прикреплённые фото, то их тоже достанем
+			if let attachments = post.attachments {
+				var images = [ApiImage]()
+				
+				for attachment in attachments {
+					if let image = attachment.photo {
+						images.append(image)
+					}
+					
+					if let link = attachment.link {
+						if let photo = link.photo {
+							images.append(photo)
+						}
+					}
+				}
+			
+				imageLinksArray = sortImage(by: "z", from: images)
+			}
+			
+			let newsModel = NewsTableViewCellModel(
+				source: source,
+				postDate: date.description,
+				postText: text ?? "",
+				newsImageNames: imageLinksArray ?? [],
+				likesModel: post.likes
+			)
+			news.append(newsModel)
+		}
+		return news
 	}
 }
 
